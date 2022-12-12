77180a203e19567ead6427379ecd7d7581a51366
DevSuelen <117091949+DevSuelen@users.noreply.github.com>
2022-12-12 10:38:56 -0300

    logica

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello Word");
+console.log("2+3");
+console.log("3+3");
+
+let nome = "Suelen Barros";
+const datadenascimento="08/02/1986";
+
+document.write('<h1 class="bem-vindo"'+nome+'>Hello world</h1>');
+document.getElementsByTagName("h1")[0].style.color="red";
+
+document.write(nome);
+document.write("<br>");
+nome="Suelen";
+document.write(nome);
+document.write(datadenascimento)
+    
